ee4d760b48137cf327a6386d926e3f1c87ba3715
Alex Ivanov <ivanov.alex1@gmail.com>
2020-06-04 12:42:47 +0300

    Created video flow

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let video = document.getElementById('video');
+
+function startVideo(){
+    navigator.getUserMedia(
+        {video: {},},
+        stream => video.srcObject=stream,
+        err => console.error(err)
+    )
+}
+startVideo();
